0080a2213dc83bc84e7bdeeac7c5a02dd9d3d164
abdelrahmanbonna <abdobonna@yahoo.com>
2020-08-15 03:29:03 +0200

    adding missing imports

--- app/Ambulance.php
+++ app/Ambulance.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class Ambulance extends Model
 {
