b1e05573e62ddf810aea6509d3ebe778243ae678
abdelrahmanbonna <abdobonna@yahoo.com>
2020-08-17 07:28:33 +0200

    seeding

--- database/seeds/AmbulanceSeeder.php
+++ database/seeds/AmbulanceSeeder.php
@@ -11,7 +11,22 @@ class AmbulanceSeeder extends Seeder
      */
     public function run()
     {
-        $ambulances=[];
+        $ambulances=[
+            [
+            'name'=>'North tagamo3 point',
+            'address'=>'north 90 street',
+            'contactno'=>'0220344489',
+            'email' =>'north90@email.com',
+            'password'=>Hash::make('1234567'),
+         ],[
+            'name'=>'South tagamo3 point',
+            'address'=>'South 90 street',
+            'contactno'=>'0220344489',
+            'email' =>'north90@email.com',
+            'password'=>Hash::make('1234567'),
+         ],
+
+        ];
         foreach ($ambulances as $a){
             Doctor::firstOrCreate($ambulance);
         }
